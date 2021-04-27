10b4c097e2962cc1d1f43e62be611eb9da0d3101
CodeClouds-Kaustav <CodeClouds-Kaustav@CISPL-IO-420>
2021-04-27 21:08:27 +0530

    test

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,102 +1,102 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "a40b0654-d1af-4e81-9937-37ff61fd2f64",
-    "feature": "To test the Login feature in huko site/application",
-    "scenario": "Check login is successfull with valid credentials.",
-    "start": 1619524027744,
-    "group": 20,
-    "content": "",
-    "tags": "@valid,",
-    "end": 1619524064214,
-    "className": "passed"
-  },
-  {
-    "id": "f3978897-d9ae-4476-9179-ac3de38010e2",
-    "feature": "To test the Login feature in huko site/application",
-    "scenario": "To check whether forgot password link is available in login page",
-    "start": 1619524027680,
-    "group": 26,
-    "content": "",
-    "tags": "@fgtpasslink,",
-    "end": 1619524064741,
-    "className": "passed"
-  },
-  {
-    "id": "98e7f6ac-366c-4efb-8f50-498901f9f2d6",
-    "feature": "To test the Login feature in huko site/application",
-    "scenario": "Check login is successfull with valid credentials.",
-    "start": 1619524027746,
-    "group": 21,
-    "content": "",
-    "tags": "@valid,",
-    "end": 1619524061464,
-    "className": "passed"
-  },
-  {
-    "id": "4cf66f64-cb54-447a-ba4d-49c494cb4677",
+    "id": "a9adb608-8931-4345-b6a7-c4ff11687ec3",
     "feature": "To test the Login feature in huko site/application",
     "scenario": "Check login is unsuccessfull with invalid credentials.",
-    "start": 1619524027746,
+    "start": 1619537746578,
     "group": 25,
     "content": "",
     "tags": "@invalid,",
-    "end": 1619524064831,
+    "end": 1619537794767,
     "className": "passed"
   },
   {
-    "id": "34a4f86d-d7c3-47c8-bd8f-0b382095588a",
-    "feature": "To test some HomePage components",
-    "scenario": "Check homepage components",
-    "start": 1619524027681,
-    "group": 18,
-    "content": "",
-    "tags": "@test,",
-    "end": 1619524066900,
-    "className": "passed"
-  },
-  {
-    "id": "4e174d74-2f91-4ee9-80a9-a211ffbb43d6",
-    "feature": "To test the Login feature in huko site/application",
-    "scenario": "Check login is unsuccessfull with invalid credentials.",
-    "start": 1619524027745,
-    "group": 24,
-    "content": "",
-    "tags": "@invalid,",
-    "end": 1619524066073,
-    "className": "passed"
-  },
-  {
-    "id": "05b7e66e-84e7-4d90-9f1e-f4e8f922bf08",
-    "feature": "To test the Login feature in huko site/application",
-    "scenario": "Check login is unsuccessfull with invalid credentials.",
-    "start": 1619524027744,
-    "group": 22,
-    "content": "",
-    "tags": "@invalid,",
-    "end": 1619524064785,
-    "className": "passed"
-  },
-  {
-    "id": "dd80c0d0-84f2-4db7-baaf-a3a0996830c6",
-    "feature": "To test the Login feature in huko site/application",
-    "scenario": "Check login is unsuccessfull with invalid credentials.",
-    "start": 1619524027741,
-    "group": 23,
-    "content": "",
-    "tags": "@invalid,",
-    "end": 1619524063581,
-    "className": "passed"
-  },
-  {
-    "id": "71c9cef1-bcde-4894-8816-d9516cae1e9a",
+    "id": "3916bba0-d6f2-4eef-a6f8-0eaacafed925",
     "feature": "To test the Login feature in huko site/application",
     "scenario": "Check login is successfull with valid credentials.",
-    "start": 1619524027743,
+    "start": 1619537746580,
     "group": 19,
     "content": "",
     "tags": "@valid,",
-    "end": 1619524066245,
+    "end": 1619537794767,
+    "className": "passed"
+  },
+  {
+    "id": "a14a585e-8e8c-4417-9172-30bdd2fbee5d",
+    "feature": "To test the Login feature in huko site/application",
+    "scenario": "Check login is unsuccessfull with invalid credentials.",
+    "start": 1619537746580,
+    "group": 22,
+    "content": "",
+    "tags": "@invalid,",
+    "end": 1619537793346,
+    "className": "passed"
+  },
+  {
+    "id": "e810f3da-43be-4654-9716-b2d27374ca4b",
+    "feature": "To test the Login feature in huko site/application",
+    "scenario": "Check login is successfull with valid credentials.",
+    "start": 1619537746582,
+    "group": 20,
+    "content": "",
+    "tags": "@valid,",
+    "end": 1619537794065,
+    "className": "passed"
+  },
+  {
+    "id": "6c53b5a4-2048-43d3-b633-45c2922c4f85",
+    "feature": "To test some HomePage components",
+    "scenario": "Check homepage components",
+    "start": 1619537746514,
+    "group": 18,
+    "content": "",
+    "tags": "@test,",
+    "end": 1619537796947,
+    "className": "passed"
+  },
+  {
+    "id": "4d463beb-662b-4e42-8793-1dcaea0fcede",
+    "feature": "To test the Login feature in huko site/application",
+    "scenario": "To check whether forgot password link is available in login page",
+    "start": 1619537746515,
+    "group": 26,
+    "content": "",
+    "tags": "@fgtpasslink,",
+    "end": 1619537792955,
+    "className": "passed"
+  },
+  {
+    "id": "a96993b3-644c-403b-8bd4-494f71839fbb",
+    "feature": "To test the Login feature in huko site/application",
+    "scenario": "Check login is unsuccessfull with invalid credentials.",
+    "start": 1619537746582,
+    "group": 23,
+    "content": "",
+    "tags": "@invalid,",
+    "end": 1619537790649,
+    "className": "passed"
+  },
+  {
+    "id": "9076d655-f244-4afa-b7db-21fe64913d41",
+    "feature": "To test the Login feature in huko site/application",
+    "scenario": "Check login is successfull with valid credentials.",
+    "start": 1619537746578,
+    "group": 21,
+    "content": "",
+    "tags": "@valid,",
+    "end": 1619537794767,
+    "className": "passed"
+  },
+  {
+    "id": "d29df20b-c86b-466d-bc6f-725d0c637c03",
+    "feature": "To test the Login feature in huko site/application",
+    "scenario": "Check login is unsuccessfull with invalid credentials.",
+    "start": 1619537746580,
+    "group": 24,
+    "content": "",
+    "tags": "@invalid,",
+    "end": 1619537793597,
     "className": "passed"
   }
 ]);
